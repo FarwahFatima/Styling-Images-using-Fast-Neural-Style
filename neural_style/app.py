6de8cb0c82c69fcf8e3df5f20a3c92e5d67bd4d0
Farwah Fatima <108923295+FarwahFatima@users.noreply.github.com>
2024-03-23 06:13:33 +0500

    Update app.py

--- neural_style/app.py
+++ neural_style/app.py
@@ -1,37 +1,3 @@
-# import streamlit as st
-# from PIL import Image
-# import utils
-# import style
-
-# st.title("PyTorch Style Transfer")
-# image = st.sidebar.selectbox(
-#     "Select image",
-#     {"amber.jpg", 'cat_.jpg', 'robot_.jpg'}
-# )
-
-# style_name = st.sidebar.selectbox(
-#     "Select Style",
-#     ("candy", 'mosaic', 'rain-princess-cropped','rain-princess', 'udnie')
-# )
-
-# model= "C:/Users/A-Tech/Desktop/building_object_detection_using_YOLO/fast_neural_style_implementation/fast_neural_style/neural_style/saved_models/" + style_name + ".pth"
-# input_image = "C:/Users/A-Tech/Desktop/building_object_detection_using_YOLO/fast_neural_style_implementation/fast_neural_style/neural_style/images/content-images/" + image
-# output_image = "C:/Users/A-Tech/Desktop/building_object_detection_using_YOLO/fast_neural_style_implementation/fast_neural_style/neural_style/images/output-images/" + style_name + "-" + image
-
-# st.write("### Source Image:")
-# img = Image.open(input_image)
-# st.image(img, width=400)
-
-# clicked = st.button("Stylized")
-
-# if clicked:
-#     model = style.load_model(model)
-#     style.stylize(model, input_image, output_image)
-
-#     st.write('### Output image:')
-#     image = Image.open(output_image)
-#     st.image(image, width=400)
-
 import streamlit as st
 from PIL import Image
 import style
